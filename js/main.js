e5d97caa9a052a51092a51b2487cb704f217b973
Trifle22 <xromchenkov99@gmail.com>
2021-04-17 13:09:56 +0300

    +formValidation

--- js/main.js
+++ js/main.js
@@ -12,3 +12,35 @@ var mySwiper = new Swiper('.swiper-container', {
 function scrollToContactMe() {
   document.querySelector('.footer').scrollIntoView();
 }
+
+maskPhone('#tel')
+
+const formButton = document.querySelector('.form-button');
+const contactsForm = document.querySelector('.contacts-form');
+const inputName = document.querySelector('.input-name');
+const inputPhone  = document.querySelector('.input-phone');
+
+formButton.disabled = true;
+
+
+const formValidation = () => {
+
+  const formElements = [...contactsForm.elements].filter(item => item.tagName.toUpperCase() === 'INPUT');
+  console.log(formElements);
+  contactsForm.addEventListener('change', event => {
+    const target = event.target;
+    if (target.matches('.input-name') && target.value.trim() == '') {
+      alert('Введите корректные данные')
+    } else if (target.matches('.input-phone') && target.value.length !== 18) {
+      alert('Введите корректный номер телефона')
+    }
+    else if (formElements[0].value.trim() !== '' && formElements[1].value.length === 18) {
+      formButton.disabled = false;
+    }
+  })
+
+}
+
+formValidation();
+
+
